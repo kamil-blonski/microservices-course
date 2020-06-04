0fa12759a2dc40975f916e45afb2e16168965923
Kamil Błoński <kamil.blonski123@gmail.com>
2020-06-04 09:44:08 +0200

    Rename project

---
+++ mssc-beer-service/src/main/java/guru/springframework/mscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package guru.springframework.mscbeerservice.repositories;
+
+import guru.springframework.mscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
