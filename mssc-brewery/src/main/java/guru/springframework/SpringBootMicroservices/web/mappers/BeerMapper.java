fbdc21503c3d0f760d41b54b7f72fe4e1bf9eade
Kamil Błoński <kamil.blonski@softiq.pl>
2020-06-05 12:13:11 +0200

    (7/78) Brewery - Use project Lombok and MapStruct

---
+++ mssc-brewery/src/main/java/guru/springframework/SpringBootMicroservices/web/mappers/BeerMapper.java
@@ -0,0 +1,17 @@
+package guru.springframework.SpringBootMicroservices.web.mappers;
+
+import guru.springframework.SpringBootMicroservices.domain.Beer;
+import guru.springframework.SpringBootMicroservices.web.model.BeerDto;
+import org.mapstruct.Mapper;
+
+/*
+Mapper generates methods required to map Beer <=> BeerDto. Mave, -> Lifecycle -> Clean & Compile.
+Methos are available in target/generated-sources
+ */
+@Mapper
+public interface BeerMapper {
+
+	BeerDto beerToBeerDto(Beer beer);
+
+	Beer beerDtoToBeer(BeerDto beerDto);
+}
